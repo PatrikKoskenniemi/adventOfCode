268309022f1860e62c441edb56410fb733cbb072
Patrik Koskenniemi <patrik_koskenniemi@hotmail.com>
2017-12-03 15:02:05 +0100

    [1 star] First star achived

--- day1.py
+++ day1.py
@@ -1,20 +1,11 @@
 #!/usr/bin/python
 
 import numpy as np
+import resource, sys
+resource.setrlimit(resource.RLIMIT_STACK, (2**29,-1))
+sys.setrecursionlimit(10**6)
 
 sum = 0
-
-def calculate( restOfArray ):
-	global sum;
-	if restOfArray.size == 0 :
-		return;
-	number = restOfArray[0]
-	restOfArray = np.delete(restOfArray,0)
-	#print(restOfArray)
-	if number == calculate(restOfArray):
-		sum = sum + number
-	return number;
-
 with open('inputDay1') as f:
 	lines = f.readlines()
 	for line in lines:
@@ -22,8 +13,23 @@ with open('inputDay1') as f:
 
 #array = np.array([1,1,2,2,3,4,1])
 array = np.append(array,array[0])
+arraysize = array.size
+
+def calculate( index ):
+#	print(index)
+	global sum;
+	#global array;
+	if arraysize == index :
+		return;
+	number = array[index]
+	#restOfArray = np.delete(restOfArray,0)
+	#print(restOfArray)
+	if number == calculate(index+1):
+		sum = sum + number
+	return number;
+
 #print(array)
-calculate(array)
+calculate(0)
 
 print(sum)
 
