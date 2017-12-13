0e894445f11f0c1617d1cb64c960d7de8561b4ef
Patrik Koskenniemi <patrik_koskenniemi@hotmail.com>
2017-12-13 19:23:29 +0100

    Refactored and added unitTest

--- day1.py
+++ day1.py
@@ -5,31 +5,30 @@ import resource, sys
 resource.setrlimit(resource.RLIMIT_STACK, (2**29,-1))
 sys.setrecursionlimit(10**6)
 
-sum = 0
-with open('inputDay1') as f:
-	lines = f.readlines()
-	for line in lines:
-		array = np.fromstring(line, dtype=int, sep=',')
+def main(inputArray):
+	global totalSum;
+	totalSum = 0
+	global arraysize, array, halfArrayIndex;
+	array = inputArray
+	arraysize = array.size
+	halfArrayIndex = int(arraysize/2-1)
+	calculate(0)
+	pass
 
-#array = np.array([1,2,3,4,5,1,2,8,9,9])
-#array = np.array([1,2,1,2])
-arraysize = array.size
-halfArrayIndex = int(arraysize/2-1)
 
 def calculate( index ):
-	global sum;
+	global totalSum;
 	if arraysize == index :
 		return array[halfArrayIndex];
 	number = array[index]
 	nextval = calculate(index+1)
 	if number == nextval:
-		sum = sum + number
+		totalSum = totalSum + number
 	compareTo = index + halfArrayIndex
 	return array[compareTo%arraysize]
 
-calculate(0)
+def getTotalSum():
+	return totalSum;
 
-print("sum: " + str(sum))
-
-
-
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ testDay1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import unittest
+import day1
+import numpy as np
+
+class TestDay1(unittest.TestCase):
+
+	def test_day1_small(self):
+		day1.main(np.array([1,2,1,2]))
+		self.assertEqual(day1.getTotalSum(), 6);
+
+	def test_day1_self(self):
+		day1.main(np.array([1,2,3,4,5,1,2,8,9,9]))
+		self.assertEqual(day1.getTotalSum(), 6);
+
+	def test_from_file(self):
+		with open('inputDay1') as f:
+			lines = f.readlines()
+		for line in lines:
+			array = np.fromstring(line, dtype=int, sep=',')
+		
+		day1.main(array)
+		self.assertEqual(day1.getTotalSum(),1146)
+
+if __name__ == '__main__':
+	unittest.main()
